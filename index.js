f8b86e56da11b8fed0b6ef910db23382c4df38f5
Tainara Amaral <tainara.rodriguesr@outlook.com>
2022-08-03 23:07:24 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/*IMPORTAÇÃO DO PACOTE EXPRESS: */
+const express = require('express');
+
+/*CRIAÇÃO DE UMA INSTANCIA EXECUTAVEL DO EXPRESS */
+const app = express();
+
+/*
+CRIAÇÃO DO SERVIDPR DE REQUISISÕES HTTP DA APLICAÇÃO PARAMETROS DO LISTEN
+1 - PORTA LÓGICA QUE RECEBE AS REQUISIÇÕES
+2 - CALLBACK - ARROW ANONIMA (OPCIONAL)
+*/
+
+app.listen(3000, ()=>{
+    console.log('SERVIDOR RODANDO EM: http://localhost:3000');
+});
